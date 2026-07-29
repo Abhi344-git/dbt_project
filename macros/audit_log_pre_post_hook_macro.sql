28caa13690ddecae422f5ff0d10d8debb80e1a47
Abhishek Bhattacharjee <ababhishek344@gmail.com>
2026-07-29 18:51:25 +0000

    dbt_bronze_with processed row and received row done

--- macros/audit_log_pre_post_hook_macro.sql
+++ macros/audit_log_pre_post_hook_macro.sql
@@ -26,7 +26,16 @@ SELECT
     CURRENT_USER(),
     'Pipeline Started'
 
-FROM {{ source(source_name, source_table) }};
+FROM {{ source(source_name, source_table) }}
+
+{% if is_incremental() %}
+WHERE INGESTED_AT >
+(
+    SELECT COALESCE(MAX(INGESTED_AT), '1900-01-01')
+    FROM {{ this }}
+)
+{% endif %}
+;
 
 {% endmacro %}
 
@@ -53,3 +62,34 @@ SET
 WHERE INVOCATION_ID = '{{ invocation_id }}';
 
 {% endmacro %}
+
+{{
+    config(
+        materialized='incremental',
+        pre_hook="{{ audit_pre_hook('DBT_Citi_1', 'F_TRANS_L') }}",
+        post_hook="{{ audit_post_hook() }}"
+    )
+}}
+
+SELECT
+    TRANSACTION_ID,
+    CUSTOMER_ID,
+    BRANCH_ID,
+    AMOUNT,
+    TRANSACTION_TYPE,
+    TRANSACTION_DATE,
+    CURRENCY,
+    CURRENT_TIMESTAMP() AS INGESTED_AT,
+    '{{ invocation_id }}' AS BATCH_ID
+
+FROM {{ source('DBT_Citi_1', 'F_TRANS_L') }}
+
+{% if is_incremental() %}
+
+WHERE INGESTED_AT >
+(
+    SELECT COALESCE(MAX(INGESTED_AT), '1900-01-01')
+    FROM {{ this }}
+)
+
+{% endif %}
